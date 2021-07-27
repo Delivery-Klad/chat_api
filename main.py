8b05c27bce30c6c5921df7b80d657cf68474abd3
Danila <dakfadeev@gmail.com>
2021-07-27 22:47:48 +0300

    add auth to api methods

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ y = yadisk.YaDisk(token=os.environ.get('yandex_token'))
 app_url = "chat-b4ckend.herokuapp.com"
 auth_handler = AuthHandler()
 ip_table, recovery_codes = [], []
-secret = os.environ.get('key')
+admin_user = os.environ.get('key')
 app_version = 2.6
 old_version = 2.4
 
@@ -88,9 +88,9 @@ def ip_thread(login: str, ip: str):
     thread.start()
 
 
-@app.get("/gen/secret", tags=["Service"])
-async def gen_hex(key: str, hex_length: int):
-    if key == secret:
+@app.get("/gen/admin_user", tags=["Service"])
+async def gen_hex(hex_length: int, login=Depends(auth_handler.decode)):
+    if login == admin_user:
         import secrets
         new_secret = secrets.token_hex(hex_length)
         return new_secret
@@ -104,10 +104,10 @@ async def api_awake():
 
 
 @app.get("/tables/create", tags=["API"])
-async def create_tables(key: str):
+async def create_tables(login=Depends(auth_handler.decode)):
     connect, cursor = db_connect()
     try:
-        if key == secret:
+        if login == admin_user:
             cursor.execute('CREATE TABLE IF NOT EXISTS users(id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,'
                            'login TEXT NOT NULL UNIQUE,'
                            'password TEXT NOT NULL,'
@@ -125,7 +125,8 @@ async def create_tables(key: str):
                            'message1 BYTEA,'
                            'read INTEGER NOT NULL)')
             cursor.execute('CREATE TABLE IF NOT EXISTS links(id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,'
-                           'longlink TEXT NOT NULL)')
+                           'longlink TEXT NOT NULL,'
+                           'target TEXT NOT NULL)')
             connect.commit()
         return True
     except Exception as e:
@@ -137,7 +138,7 @@ async def create_tables(key: str):
 
 
 @app.get("/tables/check", tags=["API"])
-async def check_tables(key: str, table: str):
+async def check_tables(table: str, login=Depends(auth_handler.decode)):
     connect, cursor = db_connect()
     try:
         if table == "messages":
@@ -151,7 +152,7 @@ async def check_tables(key: str, table: str):
                                                 "to_id": res[i][3], "message": bytes2int(res[i][4]),
                                                 "message1": bytes2int(res[i][5]), "read": res[i][6]}})
             return json_dict
-        if key == secret:
+        if login == admin_user:
             cursor.execute(f"SELECT * FROM {table}")
             return cursor.fetchall()
         return False
@@ -164,10 +165,10 @@ async def check_tables(key: str, table: str):
 
 
 @app.delete("/tables/drop", tags=["API"])
-async def drop_tables(key: str, table: str):
+async def drop_tables(table: str, login=Depends(auth_handler.decode)):
     connect, cursor = db_connect()
     try:
-        if key == secret:
+        if login == admin_user:
             cursor.execute(f"DROP TABLE {table}")
             connect.commit()
         return True
@@ -180,10 +181,10 @@ async def drop_tables(key: str, table: str):
 
 
 @app.get("/database", tags=["API"])
-async def database(key: str, query: str):
+async def database(query: str, login=Depends(auth_handler.decode)):
     connect, cursor = db_connect()
     try:
-        if key == secret:
+        if login == admin_user:
             if "select" in query.lower():
                 return "Too use select operations /tables/check"
             cursor.execute(query)
