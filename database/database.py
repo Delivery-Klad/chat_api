32b5a33d11ce8d10088bf4ae458ba536b55bcefe
Danila <dakfadeev@gmail.com>
2021-08-04 01:58:36 +0300

    fix

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("postgres_url")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
