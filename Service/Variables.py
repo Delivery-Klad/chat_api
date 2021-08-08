a8c1f42f96bfedc108aae817558de0bbe3d6b1d8
Danila <dakfadeev@gmail.com>
2021-08-08 22:54:14 +0300

    update versions

--- Service/Variables.py
+++ Service/Variables.py
@@ -4,5 +4,5 @@ import os
 auth_handler = AuthHandler()
 app_url = "chat-b4ckend.herokuapp.com"
 admin_user = os.environ.get('key')
-app_version = 3.2
-old_version = 3.0
+app_version = 3.6
+old_version = 3.4
