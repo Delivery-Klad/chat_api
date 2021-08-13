a49067e553d81694fb8068fa88152aba35ff968a
Пожилой молодой человек <49003989+Delivery-Klad@users.noreply.github.com>
2021-08-13 16:24:46 +0300

    logger

--- Service/Logger.py
+++ Service/Logger.py
@@ -1,6 +1,16 @@
-def error_log(error):  # просто затычка, будет дописано
+import linecache
+from sys import exc_info
+
+def error_log(error):
     try:
-        print(error)
+        exc_type, exc_obj, tb = exc_info()
+        _frame = tb.tb_frame
+        linenos = tb.tb_lineno
+        filename = _frame.f_code.co_filename
+        linecache.checkcache(filename)
+        line = linecache.getline(filename, linenos, _frame.f_globals)
+        reason = f"EXCEPTION IN ({filename}, LINE {linenos} '{line.strip()}'): {exc_obj}"
+        print(f"{reason}\n")
     except Exception as e:
         print(e)
         print("Возникла ошибка при обработке errorLog (Это вообще как?)")
